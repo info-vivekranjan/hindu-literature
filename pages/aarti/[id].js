d8416dc7533e6246c32f3a80a84adbae2329f42a
info-vivekranjan <infovivek.ranjan@gmail.com>
2023-08-19 15:33:05 +0530

    UI changes

--- pages/aarti/[id].js
+++ pages/aarti/[id].js
@@ -1,18 +1,19 @@
-import Head from 'next/head'
+import Head from "next/head";
 import Image from "next/image";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 import { Card } from "@mui/material";
-import { Inter } from 'next/font/google'
-import { useRouter } from 'next/router'
+import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function AartiData({posts}) {
-    const router = useRouter();
+export default function AartiData({ posts }) {
+  const router = useRouter();
 
-    const { id } = router.query;
-    console.log(id);
+  const { id } = router.query;
+  console.log(id);
   return (
     <>
       <Head>
@@ -21,36 +22,45 @@ export default function AartiData({posts}) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main style={{ display: 'flex', justifyContent:'center' }}>
-        {
-            posts?.data?.map((item)=>{
-                return <Box>
-                    <Typography variant='h5'>{item.title}</Typography>
-                    <br />
-                    <Image src={item.image} width={450} height={450}/>
-                    <br />
-                    <br />
-                    <div className="product-des" dangerouslySetInnerHTML={{ __html: item.description }}/>
+      <main>
+        <Grid container sx={{pl: '10px', pr: '10px'}}>
+          <Grid item xs={0} md={4}></Grid>
+          <Grid item xs={12} md={4}>
+            {posts?.data?.map((item) => {
+              return (
+                <Box>
+                  <Typography variant="h5">{item.title}</Typography>
+                  <br />
+                  <Image src={item.image} width={400} height={400} />
+                  <br />
+                  <br />
+                  <div
+                    className="product-des"
+                    dangerouslySetInnerHTML={{ __html: item.description }}
+                  />
                 </Box>
-            })
-        }
+              );
+            })}
+          </Grid>
+          <Grid item xs={0} md={4}></Grid>
+        </Grid>
       </main>
     </>
-  )
+  );
 }
 
 export async function getServerSideProps(param) {
-    const {id} = param.query;
-    // Call an external API endpoint to get posts.
-    // You can use any data fetching library
-    const res = await fetch(`https://hindu-literature.vercel.app/api/aarti/${id}`)
-    const posts = await res.json()
-   
-    // By returning { props: { posts } }, the Blog component
-    // will receive `posts` as a prop at build time
-    return {
-      props: {
-        posts,
-      },
-    }
-  }
+  const { id } = param.query;
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch(`http://localhost:3000/api/aarti/${id}`);
+  const posts = await res.json();
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts,
+    },
+  };
+}
--- pages/aarti/index.js
+++ pages/aarti/index.js
@@ -1,19 +1,19 @@
-import Head from 'next/head'
+import Head from "next/head";
 import Image from "next/image";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
-import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 import { Card } from "@mui/material";
-import { Inter } from 'next/font/google'
-import Link from 'next/link';
+import { Inter } from "next/font/google";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Aarti({posts}) {
+export default function Aarti({ posts }) {
   return (
     <>
       <Head>
@@ -22,50 +22,58 @@ export default function Aarti({posts}) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main style={{ width: '25%', margin: 'auto' }}>
-        <Typography variant='h4' sx={{ textAlign: 'center' }} >Aarti Sangrah</Typography>
-        <List sx={{ width: "100%", maxWidth: 500, bgcolor: "background.paper" }}>
-        {posts?.data?.map((item) => {
-          return (
-            <Box key={item.id}>
-              <Card sx={{ mb: "15px" }}>
-                <Link href={`/aarti/${item.id}`}>
-                <ListItem alignItems="flex-start">
-                  <ListItemAvatar sx={{ mr: "20px" }}>
-                    <Image
-                      alt={item.title}
-                      src={item.image}
-                      width={100}
-                      height={100}
-                    />
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={item.title}
-                    secondary={<Box>{item.card_intro}</Box>}
-                  />
-                </ListItem>
-                </Link>
-              </Card>
-            </Box>
-          );
-        })}
-      </List>
+      <main>
+        <Grid container sx={{ pl: "10px", pr: "10px" }}>
+          <Grid item xs={0} md={4}></Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h4" style={{ textAlign: 'center' }}>Aarti Sangrah</Typography>
+            <List
+              sx={{ width: "100%", maxWidth: 500, bgcolor: "background.paper" }}
+            >
+              {posts?.data?.map((item) => {
+                return (
+                  <Box key={item.id}>
+                    <Card sx={{ mb: "15px" }}>
+                      <Link href={`/aarti/${item.id}`}>
+                        <ListItem alignItems="flex-start">
+                          <ListItemAvatar sx={{ mr: "20px" }}>
+                            <Image
+                              alt={item.title}
+                              src={item.image}
+                              width={100}
+                              height={100}
+                            />
+                          </ListItemAvatar>
+                          <ListItemText
+                            primary={item.title}
+                            secondary={<Box>{item.card_intro}</Box>}
+                          />
+                        </ListItem>
+                      </Link>
+                    </Card>
+                  </Box>
+                );
+              })}
+            </List>
+          </Grid>
+          <Grid item xs={0} md={4}></Grid>
+        </Grid>
       </main>
     </>
-  )
+  );
 }
 
 export async function getServerSideProps() {
-    // Call an external API endpoint to get posts.
-    // You can use any data fetching library
-    const res = await fetch('https://hindu-literature.vercel.app/api/aarti')
-    const posts = await res.json()
-   
-    // By returning { props: { posts } }, the Blog component
-    // will receive `posts` as a prop at build time
-    return {
-      props: {
-        posts,
-      },
-    }
-  }
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch("http://localhost:3000/api/aarti");
+  const posts = await res.json();
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts,
+    },
+  };
+}
