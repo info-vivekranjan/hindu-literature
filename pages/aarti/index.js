138a2c1f83a21048036ccd20a2cd59bad9824ace
info-vivekranjan <infovivek.ranjan@gmail.com>
2023-08-20 20:41:04 +0530

    Added UI and data

--- pages/aarti/index.js
+++ pages/aarti/index.js
@@ -7,13 +7,31 @@ import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import { Card } from "@mui/material";
+import { Card, TextField } from "@mui/material";
 import { Inter } from "next/font/google";
 import Link from "next/link";
+import { useState } from "react";
+import Fab from "@mui/material/Fab";
+import SearchIcon from "@mui/icons-material/Search";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Aarti({ posts }) {
+  const [searchText, setSearchText] = useState("");
+  const [filteredData, setFilteredData] = useState(posts);
+
+  const handleSearch = async () => {
+    if (searchText) {
+      const response = await fetch(
+        `https://hindu-literature.vercel.app/api/aarti?substring=${searchText}`
+      );
+      const data = await response.json();
+      setFilteredData(data);
+    } else {
+      setFilteredData(posts);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -23,14 +41,41 @@ export default function Aarti({ posts }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Grid container sx={{ pl: "10px", pr: "10px" }}>
+        <Grid container>
           <Grid item xs={0} md={4}></Grid>
           <Grid item xs={12} md={5}>
-            <Typography variant="h4" style={{ textAlign: 'center' }}>Aarti Sangrah</Typography>
-            <List
-              sx={{ width: "100%", bgcolor: "background.paper" }}
+            <Typography variant="h4" style={{ textAlign: "center" }}>
+              {" "}
+              Aarti Sangrah
+            </Typography>
+          </Grid>
+          <Grid item xs={0} md={3}></Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={0} md={9}></Grid>
+          <Grid item xs={12} md={3}>
+            <TextField
+              variant="standard"
+              type="text"
+              placeholder="Enter a search term"
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+            <Fab
+              size="small"
+              color="primary"
+              aria-label="search"
+              onClick={handleSearch}
             >
-              {posts?.data?.map((item) => {
+              <SearchIcon />
+            </Fab>
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={0} md={4}></Grid>
+          <Grid item xs={12} md={5}>
+            <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+              {filteredData?.data?.map((item) => {
                 return (
                   <Box key={item.id}>
                     <Card sx={{ mb: "15px" }}>
@@ -63,10 +108,11 @@ export default function Aarti({ posts }) {
   );
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(param) {
+  const { substring } = param.query;
   // Call an external API endpoint to get posts.
   // You can use any data fetching library
-  const res = await fetch("https://hindu-literature.vercel.app/api/aarti");
+  const res = await fetch(`https://hindu-literature.vercel.app/api/aarti`);
   const posts = await res.json();
 
   // By returning { props: { posts } }, the Blog component
--- pages/api/aarti/index.js
+++ pages/api/aarti/index.js
@@ -1,5 +1,19 @@
 import data from '../../../utils/data.json'
 
 export default function handler(req, res) {
-    res.status(200).json({ data: data })
+    const { substring } = req.query;
+  
+    const filteredData = data?.filter(item =>
+      item?.title.toLowerCase().includes(substring?.toLowerCase())
+    );
+
+
+    let totalData;
+    if(substring){
+        totalData = filteredData
+    }else{
+        totalData = data
+    }
+
+    res.status(200).json({ data: totalData })
 }
