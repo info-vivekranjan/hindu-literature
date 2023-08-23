da3c9c2eebe5a1af284916becc87f66cf7cafa3a
info-vivekranjan <infovivek.ranjan@gmail.com>
2023-08-23 15:34:14 +0530

    UI changes

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,20 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import Link from "next/link";
+import data from "../utils/data.json";
+
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Pagination, Autoplay, Navigation } from "swiper/modules";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,10 +39,7 @@ export default function Home() {
             <Grid item xs={12} md={1}></Grid>
             <Grid item xs={12} md={2} className={styles.optionCard}>
               <Link href={"/aarti"}>
-                <Typography
-                  variant="h6"
-                  className={styles.card}
-                >
+                <Typography variant="h6" className={styles.card}>
                   {" "}
                   Aarti Sangrah
                 </Typography>
@@ -40,10 +49,7 @@ export default function Home() {
             <Grid item xs={12} md={1}></Grid>
             <Grid item xs={12} md={2} className={styles.optionCard}>
               <Link href={"/bhajan"}>
-                <Typography
-                  variant="h6"
-                  className={styles.card}
-                >
+                <Typography variant="h6" className={styles.card}>
                   {" "}
                   Bhajan Sangrah
                 </Typography>
@@ -53,10 +59,7 @@ export default function Home() {
             <Grid item xs={12} md={1}></Grid>
             <Grid item xs={12} md={2} className={styles.optionCard}>
               <Link href={"/quote"}>
-                <Typography
-                  variant="h6"
-                  className={styles.card}
-                >
+                <Typography variant="h6" className={styles.card}>
                   {" "}
                   Quote Sangrah
                 </Typography>
@@ -64,6 +67,109 @@ export default function Home() {
             </Grid>
             <Grid item xs={12} md={1}></Grid>
           </Grid>
+        </div>
+        <div style={{ marginTop: "250px", marginBottom: "50px" }}>
+          <Swiper
+            style={{
+              "--swiper-navigation-color": "#F7BE7C",
+            }}
+            slidesPerView={1}
+            spaceBetween={10}
+            autoplay={{
+              delay: 1500,
+              disableOnInteraction: false,
+            }}
+            navigation={true}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+              },
+              1024: {
+                slidesPerView: 4,
+                spaceBetween: 50,
+              },
+            }}
+            modules={[Autoplay, Navigation]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <Link href={`/aarti/${data[0].id}`}>
+                <Box className={styles.eachSlideCard}>
+                  <img src={data[0].image} />
+                  <Typography>{data[0].card_intro}</Typography>
+                </Box>
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link href={`/aarti/${data[1].id}`}>
+                <Box className={styles.eachSlideCard}>
+                  <img src={data[1].image} />
+                  <Typography>{data[1].card_intro}</Typography>
+                </Box>
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link href={`/aarti/${data[2].id}`}>
+                <Box className={styles.eachSlideCard}>
+                  <img src={data[2].image} />
+                  <Typography>{data[2].card_intro}</Typography>
+                </Box>
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link href={`/aarti/${data[3].id}`}>
+                <Box className={styles.eachSlideCard}>
+                  <img src={data[3].image} />
+                  <Typography>{data[3].card_intro}</Typography>
+                </Box>
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link href={`/aarti/${data[4].id}`}>
+                <Box className={styles.eachSlideCard}>
+                  <img src={data[4].image} />
+                  <Typography>{data[4].card_intro}</Typography>
+                </Box>
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link href={`/aarti/${data[5].id}`}>
+                <Box className={styles.eachSlideCard}>
+                  <img src={data[5].image} />
+                  <Typography>{data[5].card_intro}</Typography>
+                </Box>
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link href={`/aarti/${data[6].id}`}>
+                <Box className={styles.eachSlideCard}>
+                  <img src={data[6].image} />
+                  <Typography>{data[6].card_intro}</Typography>
+                </Box>
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link href={`/aarti/${data[7].id}`}>
+                <Box className={styles.eachSlideCard}>
+                  <img src={data[7].image} />
+                  <Typography>{data[7].card_intro}</Typography>
+                </Box>
+              </Link>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Link href={`/aarti/${data[8].id}`}>
+                <Box className={styles.eachSlideCard}>
+                  <img src={data[8].image} />
+                  <Typography>{data[8].card_intro}</Typography>
+                </Box>
+              </Link>
+            </SwiperSlide>
+          </Swiper>
         </div>
       </main>
     </>
